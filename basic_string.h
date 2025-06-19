271c8a26c44e8087d626d08157009c361157188b
tsents <tsents.sagie@gmail.com>
2025-06-19 16:10:38 +0300

    Add header declaration of the basicString class. without implementation.

---
+++ basic_string.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <ostream>
+
+/*
+ * This class implements a string class, simillar to std::string.
+ * it should behave as smooth as basic string! and be invisible to the user.
+ */
+class basicString {
+public:
+    basicString();
+    basicString(const char*);
+
+    /*
+     * Deconstructor that frees char_data from the heap.
+     */
+    ~basicString();
+
+    /*
+     * Overloads << to calls << as it will with char*.
+     */
+    friend std::ostream& operator<<(std::ostream& os, const basicString& obj);
+
+    /*
+     * Concatenates two string to create a new string.
+     */
+    friend basicString operator+(const basicString& lhs, const basicString& rhs);
+
+    /*
+     * Returns a string that is the base string repeated (times) times.
+     */
+    friend basicString operator*(const basicString& base, int times);
+
+    /**
+     * Comparision of strings based on lexigraphic ordering. (wrapping strcmp result).
+     */
+    friend bool operator==(const basicString& lhs, const basicString& rhs);
+    friend bool operator<(const basicString& lhs, const basicString& rhs);
+    friend bool operator>(const basicString& lhs, const basicString& rhs);
+    friend bool operator>=(const basicString& lhs, const basicString& rhs);
+    friend bool operator>=(const basicString& lhs, const basicString& rhs);
+
+private:
+    const char* char_data;
+};
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "basic_string.h"
+
+int main() {
+    return 0;
+}
